f8b37007656a3cff46f87bd89ca3e76d6aaaa50c
DivyaGovardhan <aslikyan2006@gmail.com>
2025-02-14 12:22:25 +0700

    Автотесты

--- src/Collect.php
+++ src/Collect.php
@@ -1,124 +1,38 @@
 <?php
 
-namespace Collect;
+namespace DivyaGovardhan\PHPUnit;
 
 class Collect
 {
-    private array $array = [];
+    protected $items;
 
-    public function __construct(array $array = [])
+    public function __construct(array $items = [])
     {
-        $this->array = $array;
+        $this->items = $items;
     }
 
-    public function keys(): Collect
+    public function keys()
     {
-        return new self(array_keys($this->array));
+        return array_keys($this->items);
     }
 
-    public function values(): Collect
+    public function values()
     {
-        return new self(array_values($this->array));
+        return array_values($this->items);
     }
 
-    public function get($key = null)
+    public function get($key)
     {
-        return $this->array[$key] ?? $this->array;
+        return $this->items[$key] ?? null;
     }
 
-    public function except(...$attrs): Collect
+    public function except(array $keys)
     {
-        if (gettype($attrs[0]) === 'array') {
-            $attrs = $attrs[0];
-        }
-        return collection(array_diff_key($this->array, array_flip($attrs)));
+        return new static(array_diff_key($this->items, array_flip($keys)));
     }
 
-    public function only(...$attrs): Collect
+    public function toArray()
     {
-        if (gettype($attrs[0]) === 'array') {
-            $attrs = $attrs[0];
-        }
-        return new self(array_intersect_key($this->array, array_flip($attrs)));
-    }
-
-    public function first()
-    {
-        return $this->array[array_key_first($this->array)];
-    }
-
-    public function count(): int
-    {
-        return count($this->array);
-    }
-
-    public function toArray(): array
-    {
-        return $this->array;
-    }
-
-    public function search($key, $value): Collect
-    {
-        $tmp = array_keys(array_column($this->array, $key), $value);
-
-        return (new self($tmp))->map(function ($idx) {
-            return $this->array[$idx];
-        });
-    }
-
-    public function map(callable $callback): Collect
-    {
-        return new self(array_map($callback, $this->array));
-    }
-
-    //3
-    public function filter(callable $callback): Collect
-    {
-        return new self(array_filter($this->array, $callback));
-    }
-
-    public function each(callable $callback, ...$args): Collect
-    {
-        foreach ($this->array as $key => $item) {
-            $callback($item, $key, ...$args);
-        }
-        return $this;
-    }
-
-    public function push($value, $key = null): Collect
-    {
-        if (gettype($value) === 'array') {
-            $value = new self($value);
-        }
-        if ($key) {
-            $this->array[$key] = $value;
-        } else {
-            $this->array[] = $value;
-        }
-        return $this;
-    }
-
-    public function unshift($value): Collect
-    {
-        array_unshift($this->array, $value);
-        return $this;
-    }
-
-    public function shift(): Collect
-    {
-        array_shift($this->array);
-        return $this;
-    }
-
-    public function pop(): Collect
-    {
-        array_pop($this->array);
-        return $this;
-    }
-
-    public function splice($idx, $length = 1): Collect
-    {
-        array_splice($idx, $length);
-        return $this;
+        return $this->items;
     }
 }
--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,45 @@
 <?php
 
+namespace DivyaGovardhan\PHPUnit;
+
 use PHPUnit\Framework\TestCase;
+use DivyaGovardhan\PHPUnit\Collect;
 
 class CollectTest extends TestCase
 {
-    public function testCount()
+    protected Collect $collection; // Явно указываем тип для коллекции
+
+    protected function setUp(): void
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        // Инициализация коллекции с тестовыми данными перед каждым тестом
+        $this->collection = new Collect([1 => 'a', 2 => 'b', 3 => 'c']);
     }
 
+    // Тест для метода keys() - проверяет, что ключи коллекции возвращаются правильно
+    public function testKeys(): void
+    {
+        $keys = $this->collection->keys();
+        $this->assertEquals([1, 2, 3], $keys, 'Ключи коллекции должны совпадать с ожидаемыми.');
+    }
+
+    // Тест для метода values() - проверяет, что значения коллекции возвращаются правильно
+    public function testValues(): void
+    {
+        $values = $this->collection->values();
+        $this->assertEquals(['a', 'b', 'c'], $values, 'Значения коллекции должны совпадать с ожидаемыми.');
+    }
+
+    // Тест для метода get() - проверяет, что значение по заданному ключу возвращается правильно
+    public function testGet(): void
+    {
+        $value = $this->collection->get(2);
+        $this->assertEquals('b', $value, 'Значение по ключу 2 должно быть "b".');
+    }
+
+    // Тест для метода except() - проверяет, что исключение указанных ключей работает корректно
+    public function testExcept(): void
+    {
+        $filtered = $this->collection->except([1, 3]);
+        $this->assertEquals([2 => 'b'], $filtered->toArray(), 'Исключенные ключи должны вернуть правильные значения.');
+    }
 }
